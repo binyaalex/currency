b2cb49f283933588d50538982933933199a1d895
unknown <binyaalex@gmail.com>
2023-08-22 16:35:24 +0300

    make option to change the main rate

--- public/script.js
+++ public/script.js
@@ -1,4 +1,38 @@
 // script.js
+const currencies = {
+    main: 'USD',
+    others: ['EUR', 'ILS', 'GBP', 'JPY'],
+};
+  
+  function populateCurrencies() {
+    const mainCurrencyElement = document.getElementById('mainCurrency');
+    const mainFlag = document.getElementById('mainFlag');
+    mainCurrencyElement.textContent = currencies.main;
+    mainFlag.src = `${currencies.main.toLowerCase()}_flag.png`;
+
+    const othersContainer = document.getElementById('othersContainer');
+    othersContainer.innerHTML = currencies.others
+      .map((currency) => `<div class="currency" id="${currency.toLowerCase()}" onclick="swapCurrencies('${currencies.main}', '${currency}')">
+        <img class="flag" src="${currency.toLowerCase()}_flag.png" alt="${currency} Flag">
+        <span id="${currency.toLowerCase()}Amount"></span>${currency}
+      </div>`)
+      .join('');
+  }
+  
+  function swapCurrencies(currentMain, selectedCurrency) {
+    const mainCurrencyElement = document.getElementById('mainCurrency');
+    const selectedCurrencyIndex = currencies.others.indexOf(selectedCurrency);
+    if (selectedCurrencyIndex !== -1) {
+      currencies.others[selectedCurrencyIndex] = currencies.main;
+      currencies.main = selectedCurrency;
+      populateCurrencies();
+    }
+  }
+  
+  // Populate currencies on page load
+  populateCurrencies();
+  
+
 console.log(1);
 const baseCurrency = document.getElementById('Amount');
 const convertButton = document.getElementById('convertButton');
@@ -36,36 +70,54 @@ async function fetchExchangeRates() {
   }
   
   // Function to perform currency conversion using exchange rates
-  function convertCurrency(amount, fromCurrency, toCurrency, exchangeRates) {
-    const fromRate = exchangeRates[fromCurrency];
-    const toRate = exchangeRates[toCurrency];
+//   function convertCurrency(amount, fromCurrency, toCurrency, exchangeRates) {
+//     const fromRate = exchangeRates[fromCurrency];
+//     const toRate = exchangeRates[toCurrency];
   
-    if (fromRate && toRate) {
-      const convertedAmount = (amount / fromRate) * toRate;
-      return convertedAmount;
-    } else {
-      console.error('Exchange rates for selected currencies not available.');
-      return null;
+//     if (fromRate && toRate) {
+//       const convertedAmount = (amount / fromRate) * toRate;
+//       return convertedAmount;
+//     } else {
+//       console.error('Exchange rates for selected currencies not available.');
+//       return null;
+//     }
+//   }
+  
+    // Function to calculate equivalent amounts for other currencies based on main currency
+    function calculateEquivalentAmounts(amount, mainCurrency, exchangeRates) {
+        console.log(amount);
+        console.log(mainCurrency);
+        console.log(exchangeRates);
+        const equivalentAmounts = {};
+        for (const currency of currencies.others) {
+            equivalentAmounts[currency] = (amount / exchangeRates[mainCurrency]) * exchangeRates[currency];
+        }
+        console.log(equivalentAmounts);
+        return equivalentAmounts;
     }
-  }
-  
+
   // Event listener for the Convert button
   document.getElementById('convertButton').addEventListener('click', async () => {
-    const amount = parseFloat(document.getElementById('Amount').value);
-    const fromCurrency = 'ILS';
-    const toCurrency = 'USD'; // Replace with the desired target currency
+    // const amount = parseFloat(document.getElementById('Amount').value);
+    const fromCurrency = currencies.main;
+    // const toCurrency = 'USD'; // Replace with the desired target currency
   
     const exchangeRates = await fetchExchangeRates();
     if (exchangeRates) {
         console.log(exchangeRates);
-        ilsAmount.textContent = (amountOfBase * exchangeRates.ILS).toFixed(2);
-        eurAmount.textContent = (amountOfBase * exchangeRates.EUR).toFixed(2);
-        gbpAmount.textContent = (amountOfBase * exchangeRates.GBP).toFixed(2);
-        jpyAmount.textContent = (amountOfBase * exchangeRates.JPY).toFixed(2);
-    //   const convertedAmount = convertCurrency(amount, fromCurrency, toCurrency, exchangeRates);
-    //   if (convertedAmount !== null) {
-    //     document.getElementById('usdAmount').textContent = convertedAmount.toFixed(2);
-    //   }
+        if (currencies.main === "USD") {
+            ilsAmount.textContent = (amountOfBase * exchangeRates.ILS).toFixed(2);
+            eurAmount.textContent = (amountOfBase * exchangeRates.EUR).toFixed(2);
+            gbpAmount.textContent = (amountOfBase * exchangeRates.GBP).toFixed(2);
+            jpyAmount.textContent = (amountOfBase * exchangeRates.JPY).toFixed(2);
+        } else {
+            const equivalentAmounts = calculateEquivalentAmounts(amountOfBase, fromCurrency, exchangeRates);
+            console.log(equivalentAmounts);
+            for (const currency of currencies.others) {
+                const mainEquivalentAmount = (amountOfBase / exchangeRates[currency]) * exchangeRates[fromCurrency];
+                document.getElementById(`${currency.toLowerCase()}Amount`).textContent = equivalentAmounts[currency].toFixed(2);
+            }
+        }
     }
   });
   
